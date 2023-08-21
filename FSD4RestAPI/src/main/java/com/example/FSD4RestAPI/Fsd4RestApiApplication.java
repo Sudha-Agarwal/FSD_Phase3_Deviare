e6ab2dea7e2b32e20f616fc7127e4a57700867e9
Sudha Agarwal <sudha.agarwal84@gmail.com>
2023-08-21 15:48:01 +0530

    FSD4_Rest_APIs

---
+++ FSD4RestAPI/src/main/java/com/example/FSD4RestAPI/Fsd4RestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.FSD4RestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fsd4RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fsd4RestApiApplication.class, args);
+	}
+
+}
