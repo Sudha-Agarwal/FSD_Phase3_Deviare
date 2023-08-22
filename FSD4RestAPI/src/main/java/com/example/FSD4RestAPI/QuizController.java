e11812735befc34b22a68de9f8fb23ac5a41c3ef
Sudha Agarwal <sudha.agarwal84@gmail.com>
2023-08-22 17:17:05 +0530

    phase4 project code

--- FSD4RestAPI/src/main/java/com/example/FSD4RestAPI/QuizController.java
+++ FSD4RestAPI/src/main/java/com/example/FSD4RestAPI/QuizController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,26 +19,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 @CrossOrigin("*")
 public class QuizController {
- @Autowired
- private QuestionRepository questionRepository;
+	@Autowired
+	private QuestionRepository questionRepository;
 
- @GetMapping("/questions")
- public List<Question> getQuestions() {
-	 List<Question> questions = questionRepository.findAll();
-     for (Question question : questions) {
-         System.out.println("Question: " + question.getQuestionText());
-     }
-     return questions;
- }
+	@GetMapping("/questions")
+	public List<Question> getQuestions() {
+		List<Question> questions = questionRepository.findAll();
+		for (Question question : questions) {
+			System.out.println("Question: " + question.getQuestionText());
+		}
+		return questions;
+	}
 
- /*@PostMapping("/submit")
+	/*@PostMapping("/submit")
  public ResponseEntity<Map<String, String>> submitAnswers(@RequestBody List<UserAnswerDTO> userAnswers) {
      Map<String, String> results = new HashMap<>();
      System.out.println((userAnswers));
-     
+
      for (UserAnswerDTO userAnswer : userAnswers) {
          Question question = questionRepository.findById(userAnswer.getQuestionId()).orElse(null);
-         
+
          if (question != null) {
              if (userAnswer.getAnswer().equalsIgnoreCase(question.getCorrectAnswer())) {
                  results.put(question.getId().toString(), "correct");
@@ -46,36 +47,47 @@ public class QuizController {
              }
          }
      }
-     
+
      return ResponseEntity.ok(results);
  }*/
- 
- @PostMapping("/submit")
- public ResponseEntity<Map<Long, String>> submitAnswers(@RequestBody Map<Long, String> userAnswers) {
-     Map<Long, String> results = new HashMap<>();
 
-     for (Map.Entry<Long, String> entry : userAnswers.entrySet()) {
-         Long questionId = entry.getKey();
-         String userAnswer = entry.getValue();
+	@PostMapping("/submit")
+	public ResponseEntity<Map<Long, String>> submitAnswers(@RequestBody Map<Long, String> userAnswers) {
+		Map<Long, String> results = new HashMap<>();
 
-         // Retrieve the correct answer from the database
-         Question question = questionRepository.findById(questionId).orElse(null);
-         if (question != null) {
-             String correctAnswer = question.getCorrectAnswer();
+		for (Map.Entry<Long, String> entry : userAnswers.entrySet()) {
+			Long questionId = entry.getKey();
+			String userAnswer = entry.getValue();
 
-             // Compare the user's answer with the correct answer
-             if (correctAnswer.equalsIgnoreCase(userAnswer)) {
-                 results.put(questionId, "correct");
-             } else {
-                 results.put(questionId, "incorrect");
-             }
-         } else {
-             // Handle case where question is not found
-             results.put(questionId, "not found");
-         }
-     }
+			// Retrieve the correct answer from the database
+			Question question = questionRepository.findById(questionId).orElse(null);
+			if (question != null) {
+				String correctAnswer = question.getCorrectAnswer();
 
-     return ResponseEntity.ok(results);
- }
- }
+				// Compare the user's answer with the correct answer
+				if (correctAnswer.equalsIgnoreCase(userAnswer)) {
+					results.put(questionId, "correct");
+				} else {
+					results.put(questionId, "incorrect");
+				}
+			} else {
+				// Handle case where question is not found
+				results.put(questionId, "not found");
+			}
+		}
+
+		return ResponseEntity.ok(results);
+	}
+	
+	@PostMapping("/addQuestion")
+	public ResponseEntity<Map<String, String>> addQuestion(@RequestBody Question question){
+		questionRepository.save(question);
+		Map<String, String> response = new HashMap<>();
+        response.put("status", "success");
+        response.put("message", "Question added successfully");  
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+
+
+}
 
